d6c0fe0ac82db8c313dafc81d219a55f7bd99fe3
Alexander Perry <yrrepxela@gmail.com>
2013-09-27 11:15:27 +0200

    Added security

---
+++ MvcApplication1/Controllers/StudentController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using ContosoUniversity.DAL;
+
+namespace MvcApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        //
+        // GET: /Student/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(
+            [Bind(Include = "LastName, FirstMidName, EnrollmentDate")]
+            Student student)
+        {
+           try
+           {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           }
+           catch (DataException /* dex */)
+           {
+               //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+               ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+           }
+
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
