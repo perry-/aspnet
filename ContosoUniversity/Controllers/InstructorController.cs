7188fd426e6224588f617ad4387817ac29809803
perry- <alexander.perry@ciber.com>
2013-09-30 15:46:09 +0200

    Lazy, eager and explicit loading

---
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -0,0 +1,151 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Instructor/
+
+        public ActionResult Index(int? id, int? courseID)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses.Select(c => c.Department))
+                .OrderBy(i => i.LastName);
+
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors.Single(i => i.InstructorID == id.Value).Courses;
+            }
+
+            if (courseID != null)
+            {
+                ViewBag.CourseID = courseID.Value;
+                var selectedCourse = viewModel.Courses.Single(x => x.CourseID == courseID);
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
+
+                viewModel.Enrollments = selectedCourse.Enrollments;
+            }
+
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Instructor/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location");
+            return View();
+        }
+
+        //
+        // POST: /Instructor/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Instructors.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+
+        //
+        // POST: /Instructor/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        //
+        // POST: /Instructor/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            db.Instructors.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
